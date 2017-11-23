5ec5853150ad89171f9d2787f068a2400fadc753
suresh <sureshajmce@gmail.com>
2017-11-23 13:34:26 +0530

    For Compatibity testing

--- IntegraPG/src/intpack/IntClass.java
+++ IntegraPG/src/intpack/IntClass.java
@@ -1,10 +1,22 @@
 package intpack;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
 public class IntClass {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws MalformedURLException {
+		System.setProperty("webdriver.chrome.driver","./driver/chromedriver.exe");
+		URL whichsystem=new URL("http://10.10.10.36:4444/wd/hub");
+		DesiredCapabilities whichbrowser=new DesiredCapabilities();
+		whichbrowser.setBrowserName("chrome");
+		WebDriver driver=new RemoteWebDriver(whichsystem,whichbrowser);
+		driver.get("https://www.google.co.in");
 	}
 
 }
