7d1ab8474bb584e287d3a2213518d9d64c7a7419
suresh <sureshajmce@gmail.com>
2017-11-23 13:36:24 +0530

    compatibility testing

--- IntegraPG/src/intpack/IntClass.java
+++ IntegraPG/src/intpack/IntClass.java
@@ -4,7 +4,6 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
